535951eb4d2e8178d4abee1eb5dbe4a99eca88a9
SavvyWalrus <92461476+SavvyWalrus@users.noreply.github.com>
2025-07-31 21:26:56 -0500

    Fixed disappearing visible classes and others alignment

--- app/overlord/sheet-generator/components/sheet-fields.tsx
+++ app/overlord/sheet-generator/components/sheet-fields.tsx
@@ -526,98 +526,98 @@ function Racials({ edit } : { edit?: boolean }) {
   const { state } = useSheetContext()
   
   return (
-    state.visibleRacialClasses ? 
-      <>
-        {
-          edit ? 
-            <>
+    <>
+      {
+        edit ? 
+          <>
+            <p
+              className="absolute top-[62.5cqw] left-[45cqw] text-[2cqw]"
+              style={deepdeneRoman.style}
+            >
+              Visible Racial Levels:
+            </p> 
+            <InputField
+              type="number"
+              field="visibleRacialClasses"
+              classname="absolute top-[62.7cqw] left-[62cqw] w-[10cqw] h-[2.7cqw] z-2"
+              min={0}
+              max={8}
+            />
+          </>
+          : 
+          (
+            state.visibleRacialClasses > 0 ?
               <p
-                className="absolute top-[62.5cqw] left-[45cqw] text-[2cqw]"
+                className="absolute top-[62.5cqw] left-[45cqw] text-[1.4cqw]"
                 style={deepdeneRoman.style}
               >
-                Visible Racial Levels:
-              </p> 
-              <InputField
-                type="number"
-                field="visibleRacialClasses"
-                classname="absolute top-[62.7cqw] left-[62cqw] w-[10cqw] h-[2.7cqw] z-2"
-                min={0}
-                max={8}
-              />
-            </>
-            : 
-            (
-              state.visibleRacialClasses > 0 ?
-                <p
-                  className="absolute top-[62.5cqw] left-[45cqw] text-[1.4cqw]"
-                  style={deepdeneRoman.style}
-                >
-                  Racial Levels
-                </p>
-                :
-                <></>
-            )
-        }
-
-        {/* Builds entries based on value of 'visibleRacialClasses' */}
-        {Array.from({ length: state.visibleRacialClasses }, (_, index) => {
-          const classTop = 62.5 + (3.78 * index)
-          const levelTop = 61.3 + (3.78 * index)
-          const paddingLeft = index > 0 ? 3 : 1
-          const leftOffset = paddingLeft > 1 ? 52.65 : 54.65
-          
-          return (
-              edit ?
-                <div key={`racial-class-edit-${index}`}>
-                  <InputField
-                    type="text"
-                    placeholder={`Racial Class ${index}...`}
-                    field="raceClasses"
-                    index={index}
-                    classname="absolute left-[44cqw] w-[16cqw] h-[2.7cqw] z-2"
-                    style={{
-                      top: `${classTop * 0.8 + 15.6}cqw`
-                    }}
-                  />
-                  <InputField
-                    type="number"
-                    min={0}
-                    max={99}
-                    field="raceLevels"
-                    index={index}
-                    classname="absolute left-[60.3cqw] w-[11cqw] h-[2.7cqw] z-2"
-                    style={{
-                      top: `${classTop * 0.8 + 15.6}cqw`
-                    }}
-                  />
-                </div>
-                :
-                <div key={`racial-class-${index}`}>
-                  <p 
-                    className={`absolute text-[1.4cqw] tracking-[0.02cqw] bg-[#eae5e2] px-1`}
-                    style={{
-                      ...deepdeneRoman.style,
-                      top: `${classTop}cqw`,
-                      paddingLeft: `${paddingLeft}cqw`,
-                      left: `${leftOffset}cqw`
-                    }}
-                  >
-                    {state.raceClasses[index]}
-                  </p>
-                  <p
-                    className={`absolute left-[79.5cqw] [word-spacing:-0.5cqw] leading-[3cqw] bg-[#eae5e2] px-1`}
-                    style={{
-                      ...OPTIPaulDavid.style,
-                      top: `${levelTop}cqw`
-                    }}
-                  >
-                    {state.raceLevels[index] > 0 ? <><span className="text-[2.7cqw] tracking-[-0.1cqw]">lvl </span><span className="text-[5cqw] tracking-[-0.8cqw]">{state.raceLevels[index]}</span></> : <></>}
-                  </p>
-                </div>
+                Racial Levels
+              </p>
+              :
+              <></>
           )
-        })}
+      }
 
-        {state.raceOthers && !edit ? (() => {
+      {/* Builds entries based on value of 'visibleRacialClasses' */}
+      {Array.from({ length: state.visibleRacialClasses }, (_, index) => {
+        const classTop = 62.5 + (3.78 * index)
+        const levelTop = 61.3 + (3.78 * index)
+        const paddingLeft = index > 0 ? 3 : 1
+        const leftOffset = paddingLeft > 1 ? 52.65 : 54.65
+        
+        return (
+            edit ?
+              <div key={`racial-class-edit-${index}`}>
+                <InputField
+                  type="text"
+                  placeholder={`Racial Class ${index}...`}
+                  field="raceClasses"
+                  index={index}
+                  classname="absolute left-[44cqw] w-[16cqw] h-[2.7cqw] z-2"
+                  style={{
+                    top: `${classTop * 0.8 + 15.6}cqw`
+                  }}
+                />
+                <InputField
+                  type="number"
+                  min={0}
+                  max={99}
+                  field="raceLevels"
+                  index={index}
+                  classname="absolute left-[60.3cqw] w-[11cqw] h-[2.7cqw] z-2"
+                  style={{
+                    top: `${classTop * 0.8 + 15.6}cqw`
+                  }}
+                />
+              </div>
+              :
+              <div key={`racial-class-${index}`}>
+                <p 
+                  className={`absolute text-[1.4cqw] tracking-[0.02cqw] bg-[#eae5e2] px-1`}
+                  style={{
+                    ...deepdeneRoman.style,
+                    top: `${classTop}cqw`,
+                    paddingLeft: `${paddingLeft}cqw`,
+                    left: `${leftOffset}cqw`
+                  }}
+                >
+                  {state.raceClasses[index]}
+                </p>
+                <p
+                  className={`absolute left-[79.5cqw] [word-spacing:-0.5cqw] leading-[3cqw] bg-[#eae5e2] px-1`}
+                  style={{
+                    ...OPTIPaulDavid.style,
+                    top: `${levelTop}cqw`
+                  }}
+                >
+                  {state.raceLevels[index] > 0 ? <><span className="text-[2.7cqw] tracking-[-0.1cqw]">lvl </span><span className="text-[5cqw] tracking-[-0.8cqw]">{state.raceLevels[index]}</span></> : <></>}
+                </p>
+              </div>
+        )
+      })}
+
+      {
+        state.raceOthers && !edit ? (() => {
           const othersTop = 61.6 + (3.78 * state.visibleRacialClasses)
 
           return (
@@ -641,52 +641,52 @@ function Racials({ edit } : { edit?: boolean }) {
               </p>
             </>
           )
-        })() : <></>}
+        })() : <></>
+      }
 
-        {
-          edit ?
-            <>
-              <p
-                className="absolute left-[45cqw] text-[2cqw]"
-                style={{
-                  ...deepdeneRoman.style,
-                  top: `${65.5 + (3.4 * state.visibleRacialClasses) - (0.4 * state.visibleRacialClasses)}cqw`
-                }}
-              >
-                Others:
-              </p>
-              <InputField
-                type=""
-                checkbox={true}
-                field="raceOthers"
-                classname="absolute left-[51.5cqw]"
-                style={{
-                  top: `${65.5 + (3.4 * state.visibleRacialClasses) - (0.4 * state.visibleRacialClasses)}cqw`
-                }}
-              />
-            </>
-            :
-            <></>
-        }
-
-        {/* Hides template lines depending on if visibleJobClasses > 0 */}
-        {!edit && state.visibleJobClasses === 0 ? Array.from({ length: (8 - state.visibleRacialClasses)}, (_, index) => {
-          const maskTop = 62.5 + (3.78 * (7 - index))
-          
-          return (
+      {
+        edit ?
+          <>
             <p
-              className="absolute left-[52cqw] text-[1.4cqw] bg-[#eae5e2] text-[#eae5e2]"
+              className="absolute left-[45cqw] text-[2cqw]"
               style={{
-                top: `${maskTop}cqw`
+                ...deepdeneRoman.style,
+                top: `${65.5 + (3.4 * state.visibleRacialClasses) - (0.4 * state.visibleRacialClasses)}cqw`
               }}
-              key={`racial-mask-${index}`}
             >
-              ■■■■■■■■■■■■■■■■■■■■■■■
+              Others:
             </p>
-          )
-        }) : <></>}
-      </> 
-    : <></>
+            <InputField
+              type=""
+              checkbox={true}
+              field="raceOthers"
+              classname="absolute left-[51.5cqw]"
+              style={{
+                top: `${66 + (3.4 * state.visibleRacialClasses) - (0.4 * state.visibleRacialClasses)}cqw`
+              }}
+            />
+          </>
+          :
+          <></>
+      }
+
+      {/* Hides template lines depending on if visibleJobClasses > 0 */}
+      {!edit && state.visibleJobClasses === 0 ? Array.from({ length: (8 - state.visibleRacialClasses)}, (_, index) => {
+        const maskTop = 62.5 + (3.78 * (7 - index))
+        
+        return (
+          <p
+            className="absolute left-[52cqw] text-[1.4cqw] bg-[#eae5e2] text-[#eae5e2]"
+            style={{
+              top: `${maskTop}cqw`
+            }}
+            key={`racial-mask-${index}`}
+          >
+            ■■■■■■■■■■■■■■■■■■■■■■■
+          </p>
+        )
+      }) : <></>}
+    </>
   )
 }
 
@@ -697,101 +697,101 @@ function Classes({ edit } : { edit?: boolean }) {
   const totalFilledClasses = state.visibleJobClasses + state.visibleRacialClasses + (state.visibleRacialClasses > 0 && state.raceOthers ? 1 : 0) + (state.jobOthers ? 1 : 0)
 
   return (
-    state.visibleJobClasses ?
-      <>
-        {
-          edit ? 
-            <>
+    <>
+      {
+        edit ? 
+          <>
+            <p
+              className="absolute top-[62.5cqw] left-[72.8cqw] text-[2cqw]"
+              style={deepdeneRoman.style}
+            >
+              Visible Racial Levels:
+            </p>
+            <InputField
+              type="number"
+              field="visibleJobClasses"
+              classname="absolute top-[62.7cqw] left-[90cqw] w-[10cqw] h-[2.7cqw] z-2"
+              min={0}
+              max={8}
+            />
+          </>
+          : 
+          (
+            state.visibleJobClasses > 0 ?
               <p
-                className="absolute top-[62.5cqw] left-[72.8cqw] text-[2cqw]"
-                style={deepdeneRoman.style}
+                className="absolute left-[45cqw] text-[1.4cqw]"
+                style={{
+                  ...deepdeneRoman.style,
+                  top: `${classTopOffset}cqw`
+                }}
               >
-                Visible Racial Levels:
+                Class Levels
               </p>
+              :
+              <></>
+          )
+      }
+
+      {/* Builds entries based on value of 'visibleJobClasses' */}
+      {Array.from({ length: state.visibleJobClasses }, (_, index) => {
+        const classTop = classTopOffset + (3.78 * index)
+        const levelTop = levelTopOffset + (3.78 * index)
+        const paddingLeft = index > 0 ? 3 : 1
+        const leftOffset = paddingLeft > 1 ? 52.65 : 54.65
+        
+        return (
+          edit ?
+            <div key={`job-class-edit-${index}`}>
+              <InputField
+                type="text"
+                placeholder={`Job Class ${index}...`}
+                field="jobClasses"
+                index={index}
+                classname="absolute left-[71.8cqw] w-[17cqw] h-[2.7cqw] z-2"
+                style={{
+                  top: `${(62.5 + (3.78 * index)) * 0.8 + 15.6}cqw`
+                }}
+              />
               <InputField
                 type="number"
-                field="visibleJobClasses"
-                classname="absolute top-[62.7cqw] left-[90cqw] w-[10cqw] h-[2.7cqw] z-2"
                 min={0}
-                max={8}
+                max={99}
+                field="jobLevels"
+                index={index}
+                classname="absolute left-[89cqw] w-[11cqw] h-[2.7cqw] z-2"
+                style={{
+                  top: `${(62.5 + (3.78 * index)) * 0.8 + 15.6}cqw`
+                }}
               />
-            </>
-            : 
-            (
-              state.visibleJobClasses > 0 ?
-                <p
-                  className="absolute left-[45cqw] text-[1.4cqw]"
-                  style={{
-                    ...deepdeneRoman.style,
-                    top: `${classTopOffset}cqw`
-                  }}
-                >
-                  Class Levels
-                </p>
-                :
-                <></>
-            )
-        }
+            </div>
+            :
+            <div key={`job-class-${index}`}>
+              <p 
+                className={`absolute text-[1.4cqw] tracking-[0.02cqw] bg-[#eae5e2] px-1`}
+                style={{
+                  ...deepdeneRoman.style,
+                  top: `${classTop}cqw`,
+                  paddingLeft: `${paddingLeft}cqw`,
+                  left: `${leftOffset}cqw`
+                }}
+              >
+                {state.jobClasses[index]}
+              </p>
+              <p
+                className={`absolute left-[79.5cqw] [word-spacing:-0.5cqw] leading-[3cqw] bg-[#eae5e2] px-1`}
+                style={{
+                  ...OPTIPaulDavid.style,
+                  top: `${levelTop}cqw`
+                }}
+              >
+                {state.jobLevels[index] > 0 ? <><span className="text-[2.7cqw] tracking-[-0.1cqw]">lvl </span><span className="text-[5cqw] tracking-[-0.8cqw]">{state.jobLevels[index]}</span></> : <></>}
+              </p>
+            </div>
+        )
+      })}
 
-        {/* Builds entries based on value of 'visibleJobClasses' */}
-        {Array.from({ length: state.visibleJobClasses }, (_, index) => {
-          const classTop = classTopOffset + (3.78 * index)
-          const levelTop = levelTopOffset + (3.78 * index)
-          const paddingLeft = index > 0 ? 3 : 1
-          const leftOffset = paddingLeft > 1 ? 52.65 : 54.65
-          
-          return (
-            edit ?
-              <div key={`job-class-edit-${index}`}>
-                <InputField
-                  type="text"
-                  placeholder={`Job Class ${index}...`}
-                  field="jobClasses"
-                  index={index}
-                  classname="absolute left-[71.8cqw] w-[17cqw] h-[2.7cqw] z-2"
-                  style={{
-                    top: `${(62.5 + (3.78 * index)) * 0.8 + 15.6}cqw`
-                  }}
-                />
-                <InputField
-                  type="number"
-                  min={0}
-                  max={99}
-                  field="jobLevels"
-                  index={index}
-                  classname="absolute left-[89cqw] w-[11cqw] h-[2.7cqw] z-2"
-                  style={{
-                    top: `${(62.5 + (3.78 * index)) * 0.8 + 15.6}cqw`
-                  }}
-                />
-              </div>
-              :
-              <div key={`job-class-${index}`}>
-                <p 
-                  className={`absolute text-[1.4cqw] tracking-[0.02cqw] bg-[#eae5e2] px-1`}
-                  style={{
-                    ...deepdeneRoman.style,
-                    top: `${classTop}cqw`,
-                    paddingLeft: `${paddingLeft}cqw`,
-                    left: `${leftOffset}cqw`
-                  }}
-                >
-                  {state.jobClasses[index]}
-                </p>
-                <p
-                  className={`absolute left-[79.5cqw] [word-spacing:-0.5cqw] leading-[3cqw] bg-[#eae5e2] px-1`}
-                  style={{
-                    ...OPTIPaulDavid.style,
-                    top: `${levelTop}cqw`
-                  }}
-                >
-                  {state.jobLevels[index] > 0 ? <><span className="text-[2.7cqw] tracking-[-0.1cqw]">lvl </span><span className="text-[5cqw] tracking-[-0.8cqw]">{state.jobLevels[index]}</span></> : <></>}
-                </p>
-              </div>
-          )
-        })}
-
-        {!edit && state.jobOthers ? (() => {
+      {
+        !edit && state.jobOthers ? (() => {
           let othersTopOffset
 
           if (state.visibleRacialClasses > 0) {
@@ -821,52 +821,52 @@ function Classes({ edit } : { edit?: boolean }) {
               </p>
             </>
           )
-        })() : <></>}
+        })() : <></>
+      }
 
-        {
-          edit ?
-            <>
-              <p
-                className="absolute left-[72.8cqw] text-[2cqw]"
-                style={{
-                  ...deepdeneRoman.style,
-                  top: `${65.5 + (3.4 * state.visibleJobClasses) - (0.4 * state.visibleJobClasses)}cqw`
-                }}
-              >
-                Others:
-              </p>
-              <InputField
-                type=""
-                checkbox={true}
-                field="jobOthers"
-                classname="absolute left-[79.2cqw]"
-                style={{
-                  top: `${65.5 + (3.4 * state.visibleJobClasses) - (0.4 * state.visibleJobClasses)}cqw`
-                }}
-              />
-            </>
-            :
-            <></>
-        }
-
-        {/* Hides template lines depending on visibleRacialClasses, visibleJobClasses, and if they show others */}
-        {!edit && Array.from({ length: (8 - totalFilledClasses)}, (_, index) => {
-          const maskTop = 62.5 + (3.78 * (7 - index))
-          
-          return (
+      {
+        edit ?
+          <>
             <p
-              className="absolute left-[52cqw] text-[1.4cqw] bg-[#eae5e2] text-[#eae5e2]"
+              className="absolute left-[72.8cqw] text-[2cqw]"
               style={{
-                top: `${maskTop}cqw`
+                ...deepdeneRoman.style,
+                top: `${65.5 + (3.4 * state.visibleJobClasses) - (0.4 * state.visibleJobClasses)}cqw`
               }}
-              key={`racial-mask-${index}`}
             >
-              ■■■■■■■■■■■■■■■■■■■■■■■
+              Others:
             </p>
-          )
-        })}
-      </>
-    : <></>
+            <InputField
+              type=""
+              checkbox={true}
+              field="jobOthers"
+              classname="absolute left-[79.2cqw]"
+              style={{
+                top: `${66 + (3.4 * state.visibleJobClasses) - (0.4 * state.visibleJobClasses)}cqw`
+              }}
+            />
+          </>
+          :
+          <></>
+      }
+
+      {/* Hides template lines depending on visibleRacialClasses, visibleJobClasses, and if they show others */}
+      {!edit && Array.from({ length: (8 - totalFilledClasses)}, (_, index) => {
+        const maskTop = 62.5 + (3.78 * (7 - index))
+        
+        return (
+          <p
+            className="absolute left-[52cqw] text-[1.4cqw] bg-[#eae5e2] text-[#eae5e2]"
+            style={{
+              top: `${maskTop}cqw`
+            }}
+            key={`racial-mask-${index}`}
+          >
+            ■■■■■■■■■■■■■■■■■■■■■■■
+          </p>
+        )
+      })}
+    </>
   )
 }
 
